2a3c93b261e5363e13bcc6ac34e9a79a6bd92cb6
lsyeon0825@gmail.com <lsyeon0825@gmail.com>
2024-06-13 23:39:57 +0900

    [design] sass 스타일링

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,15 @@
-import {FiImage} from 'react-icons/fi'
+import { FiImage } from "react-icons/fi";
+import { Link } from "react-router-dom";
 export interface PostProps {
   id: string;
   email: string;
   content: string;
   createdAt: string;
   uid: string;
+  profileUrl?: string;
+  Likes?: string[];
+  likeCount?: number;
+  comments?: any;
 }
 
 const posts: PostProps[] = [
@@ -51,7 +56,9 @@ const posts: PostProps[] = [
     uid: "123123",
   },
 ];
+
 export default function HomePage() {
+  const handelFileUpload = () => {};
   return (
     <div className="home">
       <div className="home__title">Home</div>
@@ -59,18 +66,43 @@ export default function HomePage() {
         <div className="home__tab home__tab--active">For You</div>
         <div className="home__tab">Following</div>
       </div>
+      {/* Post Form */}
       <form className="post-form">
-        <textarea className="post-form__textarea"
-        required
-        name="content"
-        placeholder="What is happening?"
+        <textarea
+          className="post-form__textarea"
+          required
+          name="content"
+          placeholder="What is happening?"
         />
         <div className="post-form__submit-area">
-          <label htmlFor="file-input" className='post-form__file'>
-            <FiImage />
+          <label htmlFor="file-input" className="post-form__file">
+            <FiImage className="post-form__file-icon" />
           </label>
+          <input
+            type="file"
+            name="file-input"
+            accept="image/*" // 파일선택을 이미지 파일만 가능하게 제한
+            onChange={handelFileUpload}
+            className="hidden"
+          />
+          <input
+            type="submit"
+            value="Tweet"
+            className="post-form__submit-btn"
+          />
         </div>
       </form>
+      {/* Tweet Post */}
+      <div className="post">
+        {posts?.map((post) => (
+          <div className="post__box" key={post?.id}>
+            <Link to={`posts/${post?.id}`}></Link>
+              <div className="post__box-profile">
+                
+              </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
