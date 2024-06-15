f529c408abf000f54f91ce973612a83b70c6474b
lsyeon0825@gmail.com <lsyeon0825@gmail.com>
2024-06-15 17:14:53 +0900

    [design] sass 스타일링

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,14 @@
-import { Children, ReactNode } from "react"
+import { Children, ReactNode } from "react";
+import MenuList from "./Menu";
 interface LayoutProps {
-    children: ReactNode; //children은 리액트가 렌더링할수있는 모든타입이 될수있음을 정의함
+  children: ReactNode; //children은 리액트가 렌더링할수있는 모든타입이 될수있음을 정의함
 }
 
-export const Layout = ({children}: LayoutProps) => {
-    return <div className="layout">{children}</div>
-}
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="layout">
+      {children}
+      <MenuList />
+    </div>
+  );
+};
