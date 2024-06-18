caaec5383806e878a265dcdf8ee80d5179e171f5
lsyeon0825@gmail.com <lsyeon0825@gmail.com>
2024-06-18 17:55:50 +0900

    feat: firebase Auth 기본 회원가입 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import { Layout } from 'components/Layout';
-import Router from 'components/Router';
-
+import { useState, useEffect } from "react";
+import { Layout } from "components/Layout";
+import Router from "components/Router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "firebaseApp";
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return <Layout>
-    <Router />
-  </Layout>
+  const auth = getAuth(app);
+  const [init, setInit] = useState<boolean>(false);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth.currentUser  // auth.currentUser가 있으면 ture, 없으면 false값으로 변환
+  );
+
+  // onAuthStateChanged: 사용자상태를 실시간으로 볼 수 있도록하는 firebase만의 실시간 업데이트 측정 함수
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {  //유저값을 받고 유저값이 있으면 setIsAuthenticated(true); 아니면  setIsAuthenticated(false);
+      if(user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true); //onAuthStateChanged 호출이 되면 항상 setInit(true);
+    })
+  },[auth]) // auth값이 바뀔때마다 호출
+
+  return (
+    <Layout>
+      <ToastContainer />
+      {/* 라우터를 바로 보여주는게 아닌 상태값이 변경되었는지 아닌지 확인*/}
+      {init ? <Router isAuthenticated={isAuthenticated}/> : "loading..."}
+    </Layout>
+  );
 }
 
 export default App;
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,32 +1,44 @@
-import HomePage from 'pages/home';
-import NotificationsPage from 'pages/notifications';
-import PostListPage from 'pages/posts';
-import PostDetail from 'pages/posts/detail';
-import PostEdit from 'pages/posts/edit';
-import PostNew from 'pages/posts/new';
-import ProfilePage from 'pages/profile';
-import ProfileEdit from 'pages/profile/edit';
-import SearchPage from 'pages/search';
-import LoginPage from 'pages/users/login';
-import SignupPage from 'pages/users/signup';
-import { Navigate, Route, Routes } from 'react-router-dom';
-export default function Router() {
+import HomePage from "pages/home";
+import NotificationsPage from "pages/notifications";
+import PostListPage from "pages/posts";
+import PostDetail from "pages/posts/detail";
+import PostEdit from "pages/posts/edit";
+import PostNew from "pages/posts/new";
+import ProfilePage from "pages/profile";
+import ProfileEdit from "pages/profile/edit";
+import SearchPage from "pages/search";
+import LoginPage from "pages/users/login";
+import SignupPage from "pages/users/signup";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+interface RouterProps {
+  isAuthenticated: boolean;
+}
+
+export default function Router({ isAuthenticated }: RouterProps) {
   return (
     <Routes>
-    <Route path="/" element={<HomePage />}/>
-    <Route path="/posts" element={<PostListPage />} />
-    <Route path="/posts/:id" element={<PostDetail />} />       {/* 개별적 아이디사용해서 게시글상세페이지로 갈수있게 경로설정 */}
-    <Route path="/posts/new" element={<PostNew />} /> 
-    <Route path="/posts/edit/:id" element={<PostEdit />} /> 
-    <Route path="/profile" element={<ProfilePage />} /> 
-    <Route path="/profile/edit" element={<ProfileEdit />} /> 
-    <Route path="/notifications" element={<NotificationsPage />} /> 
-    <Route path="/search" element={<SearchPage />} /> 
-    <Route path="/users/login" element={<LoginPage />} /> 
-    <Route path="/users/signup" element={<SignupPage />} /> 
-    <Route path="*" element={<Navigate replace to="/"/> } />
-    {/* "*" 의 의미 : 모든 패스를 받는다 = 위에서 정의한 경로가 아닌 경우엔 지정경로로 이동되게함 */}
-
-  </Routes>
-  )
+      {isAuthenticated ? (
+        <>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/posts" element={<PostListPage />} />
+          <Route path="/posts/:id" element={<PostDetail />} />{" "}
+          {/* 개별적 아이디사용해서 게시글상세페이지로 갈수있게 경로설정 */}
+          <Route path="/posts/new" element={<PostNew />} />
+          <Route path="/posts/edit/:id" element={<PostEdit />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/profile/edit" element={<ProfileEdit />} />
+          <Route path="/notifications" element={<NotificationsPage />} />
+          <Route path="/search" element={<SearchPage />} />
+        </>
+      ) : (
+        <>
+          <Route path="/users/login" element={<LoginPage />} />
+          <Route path="/users/signup" element={<SignupPage />} />
+          <Route path="*" element={<Navigate replace to="/users/login" />} />
+          {/* "*" 의 의미 : 모든 패스를 받는다 = 위에서 정의한 경로가 아닌 경우엔 지정경로로 이동되게함 */}
+        </>
+      )}
+    </Routes>
+  );
 }
