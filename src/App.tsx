43495c27fafdd8928132086c67d91f221998216e
lsyeon0825@gmail.com <lsyeon0825@gmail.com>
2024-06-19 16:01:29 +0900

    feat: LoginForm 추가 , toastify 스타일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,40 @@ import { Layout } from "components/Layout";
 import Router from "components/Router";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from "firebaseApp";
-import { ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Loader from "components/loader/Loader";
 
 function App() {
   const auth = getAuth(app);
   const [init, setInit] = useState<boolean>(false);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
-    !!auth.currentUser  // auth.currentUser가 있으면 ture, 없으면 false값으로 변환
+    !!auth.currentUser // auth.currentUser가 있으면 ture, 없으면 false값으로 변환
   );
 
   // onAuthStateChanged: 사용자상태를 실시간으로 볼 수 있도록하는 firebase만의 실시간 업데이트 측정 함수
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {  //유저값을 받고 유저값이 있으면 setIsAuthenticated(true); 아니면  setIsAuthenticated(false);
-      if(user) {
+    onAuthStateChanged(auth, (user) => {
+      //유저값을 받고 유저값이 있으면 setIsAuthenticated(true); 아니면  setIsAuthenticated(false);
+      if (user) {
         setIsAuthenticated(true);
       } else {
         setIsAuthenticated(false);
       }
       setInit(true); //onAuthStateChanged 호출이 되면 항상 setInit(true);
-    })
-  },[auth]) // auth값이 바뀔때마다 호출
+    });
+  }, [auth]); // auth값이 바뀔때마다 호출
 
   return (
     <Layout>
-      <ToastContainer />
+      <ToastContainer
+        theme="dark"
+        autoClose={1000}
+        hideProgressBar
+        newestOnTop
+      />
       {/* init 일때만 라우터가 보여지게 함 */}
-      {init ? <Router isAuthenticated={isAuthenticated}/> : <Loader />}
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </Layout>
   );
 }
--- src/components/users/LoginForm.tsx
+++ src/components/users/LoginForm.tsx
@@ -1,3 +1,102 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { app } from "firebaseApp";
+import { toast } from "react-toastify";
+
 export default function LoginForm() {
-  return <h1>LoginForm</h1>;
+  const [error, setError] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const navigate = useNavigate();
+
+  // firebase 회원가입
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+    try{
+      const auth = getAuth(app);
+      await signInWithEmailAndPassword(auth, email, password)
+      navigate("/"); //회원가입완료(=로그인) 후 메인페이지로 이동
+      toast.success("성공적으로 로그인이 되었습니다.");
+    } catch (error: any) {
+      toast.error(error?.code);
+    }
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === "email") {
+      setEmail(value);
+      const validRegex =
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~\-]+@[a-zA-Z0-9\-]+(?:\.[a-zA-Z0-9\-]+)*$/;
+
+      if (!value?.match(validRegex)) {
+        setError("이메일 형식이 올바르지 않습니다.");
+      } else {
+        setError("");
+      }
+    }
+
+    if (name === "password") {
+      setPassword(value);
+      if (value?.length < 8) {
+        setError("비밀번호는 8자리 이상 입력해주세요.");
+      } else {
+        setError("");
+      }
+    }
+  };
+  return (
+    <form className="form form--lg" onSubmit={onSubmit}>
+      <div className="form__title">로그인</div>
+      <div className="form__block">
+        <label htmlFor="email">이메일</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          value={email}
+          onChange={onChange}
+          required
+        />
+      </div>
+      <div className="form__block">
+        <label htmlFor="password">비밀번호</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={onChange}
+          required
+        />
+      </div>
+      
+      {/* 에러가있고, 에러의 length가 0보다 크면 해당에러를 보여지게 */}
+      {error && error?.length > 0 && (
+        <div className="form__block">
+          <div className="form__error">{error}</div>
+        </div>
+      )}
+
+      <div className="form__block">
+        계정이 없으신가요?
+        <Link to="/users/signup" className="form__link">
+          회원가입
+        </Link>
+      </div>
+      <div className="form__block--lg">
+        <button
+          type="submit"
+          className="form__btn--submit"
+          disabled={error?.length > 0}
+        >
+          로그인
+        </button>
+      </div>
+    </form>
+  );
 }
